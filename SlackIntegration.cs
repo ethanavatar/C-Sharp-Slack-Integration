366358729bbeb105e7ca75bc02e59197f2575084
Ethan <ethanalexevans@gmail.com>
2021-06-17 14:45:37 -0700

    Initial code upload

---
+++ SlackIntegration.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using System.IO;
+
+namespace SlackIntegration
+{
+    public class SlackBot
+    {
+        public int Send(string url, string data)
+        {
+            var httpRequest = (HttpWebRequest)WebRequest.Create(url);
+
+            httpRequest.Method = "POST";
+            httpRequest.ContentType = "application/json";
+
+            using (var streamWriter = new StreamWriter(httpRequest.GetRequestStream()))
+            {
+                streamWriter.Write("{\"text\":\"" + data + "\"}");
+            }
+
+            var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+            }
+
+            return (int)httpResponse.StatusCode;
+        }
+    }
+}
